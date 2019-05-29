5893a951292b07005d43ef2a5fce64292f2b98e0
tpr1992 <terence.pataneronan@gmail.com>
2019-05-29 17:03:34 -0400

    even more shit done

--- src/index.js
+++ src/index.js
@@ -2,46 +2,71 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   let canvasContId = document.querySelector('#canvas-container')
   let mainTextInput = document.querySelector('.text-inputs')
+  let userNameForm = document.querySelector('.user-name')
+  userSession = "";
+
   putShitOnDom(canvasContId)
 
   mainTextInput.addEventListener('input', (event) => {
     let img = window.base_pic
     let topTextInput = document.querySelector('#topText').value
+    let topCharCount = topTextInput.length
     let bottomTextInput = document.querySelector('#bottomText').value
+    let bottomCharCount = bottomTextInput.length
     reDrawImage(img, topTextInput, bottomTextInput)
   })
+
+  userNameForm.addEventListener('submit', getUserName)
 
 
 })
 
-
-// function textChangeListener(inputId, inputText){
-//   let img = window.base_pic
-//   let newTopText = "";
-//   let newBottomText = "";
-//   if (inputId === "topText") {
-//     newTopText = inputText;
-//   } else {
-//     newBottomText = inputText;
-//   }
-//   reDrawImage(img, newTopText, newBottomText)
-//   // redrawMeme(window.imageSrc, window.topText, window.bottomText);
-// }
-
 const makeCanvas = () => {
-  return `<canvas id="canvas" width="768" height="768">
+  return `<canvas id="canvas" width="512" height="512">
   </canvas>
   <img id="base_pic" src="http://ohsohumorous.com/var/albums/blank-meme-template-base/blank-meme-template-base-00313.jpg?m=1490466615"/>`
+}
+
+
+
+function getUserName() {
+  event.preventDefault()
+  let userNameValue = document.querySelector('#user-name-input').value
+  console.log(userNameValue)
+  fetch('http://localhost:3000/api/v1/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: userNameValue
+    })
+  })
+  .then(function(response) {
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    return response
+  })
+  .then(function(response){
+    createUserSession()
+  })
+  .catch(function(error){
+    createUserSession()
+  })
+}
+
+const createUserSession = () => {
+  console.log('hi')
 }
 
 function putShitOnDom(canvasContId){
   canvasContId.innerHTML = makeCanvas()
   const img = document.getElementById('base_pic');
   img.addEventListener("load", function() {
-    // debugger
     drawImage(img)
   })
-  // debugger
 }
 
 function reDrawImage(img, newTopText, newBottomText) {
@@ -66,9 +91,9 @@ function reDrawImage(img, newTopText, newBottomText) {
   else {
     ctx.font = "20px impact";
   }
-  ctx.textBaseline = "top";
-  ctx.strokeText(newTopText, 200, 10);
-  ctx.fillText(newTopText, 200, 10);
+  ctx.textBaseline = "center";
+  ctx.strokeText(newTopText, 256, 100);
+  ctx.fillText(newTopText, 256, 100);
 
   if (newBottomText.length < 15) {
     ctx.font = "60px impact";
@@ -78,9 +103,9 @@ function reDrawImage(img, newTopText, newBottomText) {
     ctx.font = "20px impact";
   }
 
-  ctx.textBaseline = "bottom";
-  ctx.strokeText(newBottomText, 200, 390);
-  ctx.fillText(newBottomText, 200, 390);
+  ctx.textBaseline = "center";
+  ctx.strokeText(newBottomText, 256, 460);
+  ctx.fillText(newBottomText, 256, 460);
 }
 
 function drawImage(img, newTopText) {
